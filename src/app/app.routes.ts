f590f34df51993844da41a8adedc6190e79f1287
Giovanni <giodirosaartes@gmail.com>
2026-03-06 12:35:05 -0300

    [feat]: Implement a basic counter page with signal and regular counter functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { CountPageComponent } from './count/counter.components';
+import { CountPageComponent } from './pages/count/counter.components';
 import { HeroPageComponent } from './pages/hero/hero.components';
 
 export const routes: Routes = [
     {
         path: '',
         component: CountPageComponent
-    }, 
+    },
     {
-         path: 'hero',
+        path: 'hero',
         component: HeroPageComponent
-  }
+    },
+    {
+        path: "**",
+        redirectTo: ""
+    }
 ];
 
