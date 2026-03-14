c36ece2724b637c9e343e703ba04478b3f22c42d
vgarces1998 <vgarces@flagare.cl>
2026-03-14 20:39:59 -0300

    [feat]: Add character example (inputs)

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page.component',
   imports: [],
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+
+  name = signal('Gohan')
+  power = signal(0)
+  
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', power: 10000},
+    {id: 2, name: 'Vegeta', power: 9000},
+    {id: 3, name: 'Piccolo', power: 8000},
+    {id: 4, name: 'Gohan', power: 7000},
+  ]);
+
+  addCharacter(){
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+    this.characters.update(currentCharacters => [...currentCharacters, newCharacter]);
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
