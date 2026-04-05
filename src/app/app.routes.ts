17c12ce2afc4983c7051fb84c89d39bdac5461ff
GIODIROSA <giodirosaartes@gmail.com>
2026-04-05 19:18:29 -0400

    [feat]: implement event and event-admin pages with navigation routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CountPageComponent } from './pages/count/counter.components';
 import { HeroPageComponent } from './pages/hero/hero.components';
 import { DragonBallPageComponent } from './pages/dragonBall/dragonBall.component';
 import { EventPageComponent } from './pages/event/event';
+import { EventAdmin } from './pages/event-admin/event-admin';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
     {
         path: 'event',
         component: EventPageComponent
+    },
+    {
+        path: 'admin',
+        component: EventAdmin
     },
     {
         path: "**",
---
+++ src/app/pages/event-admin/event-admin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-admin',
+  imports: [],
+  templateUrl: './event-admin.html',
+  styleUrl: './event-admin.css',
+})
+export class EventAdmin {}
